7232a477060ed9d23efa73833ecf0af24cebcc91
marijnschilling <marijnschilling@gmail.com>
2017-02-13 17:05:43 +0100

    Use a scrollview for the top to botom navigation

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -9,9 +9,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        let controller = LiftNavigationController(topViewController: UIViewController(), bottomViewControllers: [UIViewController]())
 
+        let topViewController = UIViewController()
+        topViewController.view.backgroundColor = .darkGray
+
+        let firstViewController = UIViewController()
+        firstViewController.view.backgroundColor = .gray
+
+        let secondViewController = UIViewController()
+        secondViewController.view.backgroundColor = .lightGray
+
+        let controller = LiftNavigationController(topViewController: topViewController, bottomViewControllers: [firstViewController, secondViewController])
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = self.window else { fatalError("Window not found") }
+
+        window.rootViewController = controller
+
+        window.makeKeyAndVisible()
+        
         return true
     }
 }
-
--- Sources/LiftNavigationController.swift
+++ Sources/LiftNavigationController.swift
@@ -1,14 +1,39 @@
 import UIKit
 
 public class LiftNavigationController: UIViewController {
-    static let navigationBarHeight = CGFloat(44.0)
+    public static let navigationBarHeight = CGFloat(44.0)
 
     public var topViewController: UIViewController
-//    public var bottomScrollView: UIScrollView
 
-    public var bottomViewControllers: [UIViewController]
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.bounces = false
 
-//    public var navigationBar: UICollectionView
+        return scrollView
+    }()
+
+    lazy var bottomScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+
+        return scrollView
+    }()
+
+    var bottomViewControllers: [UIViewController]
+
+    lazy var navigationBar: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 100, height: LiftNavigationController.navigationBarHeight)
+
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        return collectionView
+    }()
 
     public init(topViewController: UIViewController, bottomViewControllers: [UIViewController]) {
         self.topViewController = topViewController
@@ -16,7 +41,8 @@ public class LiftNavigationController: UIViewController {
 
         super.init(nibName: nil, bundle: nil)
 
-//        self.navigationBar.backgroundColor = .black
+        self.topViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        self.view.translatesAutoresizingMaskIntoConstraints = false
     }
 
     public required init?(coder aDecoder: NSCoder) {
@@ -26,31 +52,37 @@ public class LiftNavigationController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-//        self.addSubviewsAndConstraints()
+
+        self.addSubviewsAndConstraints()
     }
 
-//    func addSubviewsAndConstraints() {
-//        self.addChildViewController(self.topViewController)
-//        self.addChildViewController(self.navigationController)
-//        self.addChildViewController(self.bottomScrollView)
-//
-//        self.view.addSubview(self.topViewController)
-//        self.view.addSubview(self.navigationController)
-//        self.view.addSubview(self.bottomScrollView)
-//
-//        self.topViewController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-//        self.topViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//        self.topViewController.view.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-//        self.topViewController.view.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: -LiftNavigationController.navigationBarHeight).isActive = true
-//
-//        self.navigationBar.view.topAnchor.constraint(equalTo: self.topViewController.view.bottomAnchor).isActive = true
-//        self.navigationBar.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//        self.navigationBar.view.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-//        self.navigationBar.view.heightAnchor.constraint(equalToConstant: LiftNavigationController.navigationBarHeight).isActive = true
-//
-//        self.bottomScrollView.view.topAnchor.constraint(equalTo: self.navigationBar.bottomAnchor).isActive = true
-//        self.bottomScrollView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-//        self.bottomScrollView.view.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-//        self.bottomScrollView.view.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: -LiftNavigationController.navigationBarHeight).isActive = true
-//    }
+    func addSubviewsAndConstraints() {
+        self.addChildViewController(self.topViewController)
+
+        self.view.addSubview(self.scrollView)
+        self.scrollView.addSubview(self.topViewController.view)
+        self.scrollView.addSubview(self.navigationBar)
+        self.scrollView.addSubview(self.bottomScrollView)
+
+        self.scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.scrollView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.scrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        self.scrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
+
+        self.topViewController.view.topAnchor.constraint(equalTo: self.scrollView.topAnchor).isActive = true
+        self.topViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.topViewController.view.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        self.topViewController.view.heightAnchor.constraint(equalToConstant: self.view.bounds.height - LiftNavigationController.navigationBarHeight).isActive = true
+
+        self.navigationBar.topAnchor.constraint(equalTo: self.topViewController.view.bottomAnchor).isActive = true
+        self.navigationBar.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.navigationBar.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        self.navigationBar.heightAnchor.constraint(equalToConstant: LiftNavigationController.navigationBarHeight).isActive = true
+
+        self.bottomScrollView.topAnchor.constraint(equalTo: self.navigationBar.bottomAnchor).isActive = true
+        self.bottomScrollView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.bottomScrollView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        self.bottomScrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: -LiftNavigationController.navigationBarHeight).isActive = true
+        self.bottomScrollView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor).isActive = true
+    }
 }
