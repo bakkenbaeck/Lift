abc93e992869218dca3485522df403775fb176fc
marijnschilling <marijnschilling@gmail.com>
2017-02-15 17:04:25 +0100

    Add a button to switch

--- Sources/LiftNavigationController.swift
+++ Sources/LiftNavigationController.swift
@@ -8,8 +8,21 @@ public class LiftNavigationController: UIViewController {
         case bottom
     }
     public var topViewController: UIViewController
-    fileprivate var shouldEvaluatePageChange = false
-    var currentFloor = Floor.top
+
+    var shouldEvaluatePageChange = false
+    var currentFloor = Floor.top {
+        didSet {
+            var origin = self.view.bounds.origin
+            switch self.currentFloor {
+            case .top:
+                origin.y = 0
+            case .bottom:
+                origin.y = self.view.bounds.height - LiftNavigationController.navigationBarHeight
+            }
+
+            scrollView.setContentOffset(origin, animated: true)
+        }
+    }
 
     lazy var scrollView: UIScrollView = {
        let scrollView = UIScrollView()
@@ -30,6 +43,14 @@ public class LiftNavigationController: UIViewController {
         scrollView.backgroundColor = .white
 
         return scrollView
+    }()
+
+    lazy var switchButton: UIButton = {
+        let button = UIButton(type: .contactAdd)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didSelectSwitchButton), for: .touchUpInside)
+
+        return button
     }()
 
     lazy var navigationBar: UICollectionView = {
@@ -71,6 +92,7 @@ public class LiftNavigationController: UIViewController {
         self.view.addSubview(self.scrollView)
         self.scrollView.addSubview(self.topViewController.view)
         self.scrollView.addSubview(self.navigationBar)
+        self.navigationBar.addSubview(self.switchButton)
         self.scrollView.addSubview(self.bottomScrollView)
 
         self.scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
@@ -88,11 +110,20 @@ public class LiftNavigationController: UIViewController {
         self.navigationBar.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
         self.navigationBar.heightAnchor.constraint(equalToConstant: LiftNavigationController.navigationBarHeight).isActive = true
 
+        self.switchButton.topAnchor.constraint(equalTo: self.navigationBar.topAnchor).isActive = true
+        self.switchButton.leftAnchor.constraint(equalTo: self.navigationBar.leftAnchor).isActive = true
+        self.switchButton.heightAnchor.constraint(equalTo: self.navigationBar.heightAnchor).isActive = true
+        self.switchButton.widthAnchor.constraint(equalTo: self.navigationBar.heightAnchor).isActive = true
+
         self.bottomScrollView.topAnchor.constraint(equalTo: self.navigationBar.bottomAnchor).isActive = true
         self.bottomScrollView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         self.bottomScrollView.widthAnchor.constraint(greaterThanOrEqualTo: self.view.widthAnchor).isActive = true
         self.bottomScrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: -LiftNavigationController.navigationBarHeight).isActive = true
         self.bottomScrollView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor).isActive = true
+    }
+
+    func didSelectSwitchButton() {
+        self.currentFloor = self.currentFloor == .top ? .bottom : .top
     }
 }
 
@@ -107,20 +138,9 @@ extension LiftNavigationController: UIScrollViewDelegate {
 
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if shouldEvaluatePageChange {
-            let pageHeight = UIScreen.main.bounds.height
+            let pageHeight = self.view.bounds.height
             let index = Int(floor((scrollView.contentOffset.y - pageHeight / 2) / pageHeight) + 1)
-            print(index)
             self.currentFloor = Floor(rawValue: index) ?? self.currentFloor
-
-            var origin = self.view.bounds.origin
-            switch self.currentFloor {
-            case .top:
-                origin.y = 0
-            case .bottom:
-                origin.y = pageHeight - 64
-            }
-
-            scrollView.setContentOffset(origin, animated: true)
         }
     }
 }
